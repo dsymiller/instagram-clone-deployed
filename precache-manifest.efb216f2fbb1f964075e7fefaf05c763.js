c039b69cc5ae6734028f7fc333159e46d81062cc
Derek Miller <dsymiller@gmail.com>
2020-10-04 21:17:14 -0400

    Updates

---
+++ precache-manifest.efb216f2fbb1f964075e7fefaf05c763.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c19fcb026d97e188d1c68ef4dd9722e",
+    "url": "/dsymiller/instagram-clone/index.html"
+  },
+  {
+    "revision": "28edac043cadd7b3d9e3",
+    "url": "/dsymiller/instagram-clone/static/css/main.c57c5058.chunk.css"
+  },
+  {
+    "revision": "ac7ee313912d0c63be27",
+    "url": "/dsymiller/instagram-clone/static/js/2.e2c11421.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/dsymiller/instagram-clone/static/js/2.e2c11421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28edac043cadd7b3d9e3",
+    "url": "/dsymiller/instagram-clone/static/js/main.503693de.chunk.js"
+  },
+  {
+    "revision": "d20aba44008ae1dd82e5",
+    "url": "/dsymiller/instagram-clone/static/js/runtime-main.7d344df0.js"
+  },
+  {
+    "revision": "38825c9d5aa205d7a08cd2f2674b4f69",
+    "url": "/dsymiller/instagram-clone/static/media/phone.38825c9d.png"
+  },
+  {
+    "revision": "f5ae123ab1e24e72615bea84fc7b4845",
+    "url": "/dsymiller/instagram-clone/static/media/slide1.f5ae123a.jpg"
+  },
+  {
+    "revision": "2d9d7248af43c6a4405960bfb0254d48",
+    "url": "/dsymiller/instagram-clone/static/media/slide2.2d9d7248.jpg"
+  },
+  {
+    "revision": "629d23a3c7b24459b2584bddb8a4a8e5",
+    "url": "/dsymiller/instagram-clone/static/media/slide3.629d23a3.jpg"
+  },
+  {
+    "revision": "001bc33056c10fdbbdb1db41009b57e1",
+    "url": "/dsymiller/instagram-clone/static/media/slide4.001bc330.jpg"
+  },
+  {
+    "revision": "aafd8c6b005d7d971fad0012daa374d3",
+    "url": "/dsymiller/instagram-clone/static/media/slide5.aafd8c6b.jpg"
+  }
+]);
